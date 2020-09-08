fb8334562c07bd60d50a3484ffdceeac28b6d00e
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-08 22:50:32 +0300

    08092020

--- sim800l.py
+++ sim800l.py
@@ -12,11 +12,12 @@ class Sim800L:
     def initialization(self):
         self.command('AT\n')
         self.command('ATE0\n')
+        self.command('AT+CLIP=1\n')
 
     def command(self, cmd):
         self.serial_connection.write(codecs.encode(cmd))
         rcv = self.serial_connection.readline()
-        print(codecs.decode(rcv))
+        print(f'{cmd}:{codecs.decode(rcv)}')
 
 
 if __name__ == '__main__':
