9c88214c9104a38163e9bdadc47b48a2c62cacbd
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-11 19:07:39 +0300

    11092020

--- sim800l.py
+++ sim800l.py
@@ -11,13 +11,40 @@ class Sim800L:
 
     def initialization(self):
         self.command('AT\n')
-        self.command('ATE0\n')
-        self.command('AT+CLIP=1\n')
+        self.command('ATE0\n')  # Set Command Echo Mode Off
+        self.command('ATI\n')  # Display Product Identification Information
+        self.command('AT&V\n')  # Display Current Configuration
+        self.command('AT+GSN\n')  # Request TA Serial Number Identification(IMEI)
+        # AT+CPBR=? Read Current Phonebook Entries
+        self.command('AT+COPS?\n')  # Currently Selected Operator
+        self.command('AT+CSQ\n')  # Signal Quality Report
+        self.command('AT+CNUM\n')  # Subscriber Number
+        self.command('AT+CMGF=1\n')  # Select SMS Message Format: Text mode
 
     def command(self, cmd):
         self.serial_connection.write(codecs.encode(cmd))
         rcv = self.serial_connection.readline()
         print(f'{cmd}:{codecs.decode(rcv)}')
+
+    def check_incoming(self):
+        if self.serial_connection.in_waiting:
+            rcv = self.serial_connection.readline()
+            print(buf)
+            # buf = convert_to_string(buf)
+            rcv = codecs.decode(rcv)
+            # params = buf.split(',')
+            #
+            # if params[0][0:5] == "+CMTI":
+            #     self._msgid = int(params[1])
+            #     if self.msg_action:
+            #         self.msg_action()
+            #
+            # elif params[0] == "NO CARRIER":
+            #     self.no_carrier_action()
+            #
+            # elif params[0] == "RING" or params[0][0:5] == "+CLIP":
+            #     # @todo handle
+            #     pass
 
 
 if __name__ == '__main__':
