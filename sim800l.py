8658811d5e5e735bb0dad6050381df8f38ce69bd
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-12 12:25:37 +0300

    12092020

--- sim800l.py
+++ sim800l.py
@@ -4,6 +4,10 @@ from time import sleep
 import serial
 
 
+def decode_to_string(rcv):
+    return codecs.decode(rcv)
+
+
 class Sim800L:
 
     def __init__(self, connection='/dev/ttyAMA0'):
@@ -13,6 +17,7 @@ class Sim800L:
     def initialization(self):
         self.command('AT\n')
         self.command('ATE0\n')  # Set Command Echo Mode Off
+        self.command('AT+CSCLK=0\n')  # Disabel Slow Clock
         self.command('ATI\n')  # Display Product Identification Information
         # self.command('AT&V\n')  # Display Current Configuration
         self.command('AT+GSN\n')  # Request TA Serial Number Identification(IMEI)
@@ -25,8 +30,7 @@ class Sim800L:
     def command(self, cmd):
         self.serial_connection.write(codecs.encode(cmd))
         rcv = self.serial_connection.readline()
-        # print(f'{cmd}:{codecs.decode(rcv)}')
-        print('{0}:{1}'.format(cmd, codecs.decode(rcv)))
+        print('{0}:{1}'.format(cmd, decode_to_string(rcv)))
         sleep(1)
 
     def check_incoming(self):
@@ -36,22 +40,13 @@ class Sim800L:
             rcv = self.serial_connection.readline()
             print(rcv)
             # buf = convert_to_string(buf)
-            rcv = codecs.decode(rcv)
+            rcv = decode_to_string(rcv)
             print(rcv)
             params = rcv.split(',')
             print('PARAMS: ', params)
 
-            if params[0][0:5] == "+CMTI":
-                self._msgid = int(params[1])
-                if self.msg_action:
-                    self.msg_action()
-
-            elif params[0] == "NO CARRIER":
-                self.no_carrier_action()
-
-            elif params[0] == "RING" or params[0][0:5] == "+CLIP":
+            if params[0] == "RING" or params[0][0:5] == "+CLIP":
                 phone_number = params[0].strip().split(':')[1]
-                # print('CALL FROM {}'.format(phone_number))
                 sleep(1)
                 self.command('ATH\n')
                 sleep(1)
