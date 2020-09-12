3742158aab4b73f6472abc38e3f0e0a82d30f8ee
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-12 10:49:11 +0300

    12092020

--- sim800l.py
+++ sim800l.py
@@ -50,7 +50,7 @@ class Sim800L:
                 self.no_carrier_action()
 
             elif params[0] == "RING" or params[0][0:5] == "+CLIP":
-                print('CALL FROM {}', params[0].strip(' '))
+                print('CALL FROM {}'.format(params[0].strip().split(':')[1]))
 
 
 if __name__ == '__main__':
